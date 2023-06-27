d3a3f3f4c01300d5372caf48b3153c64073657c5
Maria Paz Abarca <mp.abarca.alvear@gmail.com>
2023-06-27 13:38:36 +0200

    Add getRandomImageByBreed service to BreedRandomImage component + test

--- src/components/molecules/BreedRandomImage/BreedRandomImage.tsx
+++ src/components/molecules/BreedRandomImage/BreedRandomImage.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import {ResponseRandomImageByBreed} from '../../../common/responseTypes';
+import BreedImage from '../../atoms/BreedImage/BreedImage';
+import {getRandomImageByBreed} from '../../../services/random-image-by-breed';
+import React, {useEffect, useState} from 'react';
 
 export interface BreedRandomImageProps {
   breedName: string;
 }
 
 const BreedRandomImage: React.FC<BreedRandomImageProps> = ({breedName}) => {
-  return <div>{breedName}</div>;
+  const [breedImage, setBreedImage] = useState<string>('');
+  const [status, setStatus] = useState<boolean>(true);
+
+  const getData = async () => {
+    try {
+      const response: ResponseRandomImageByBreed = await getRandomImageByBreed(breedName);
+      setBreedImage(response.message);
+      setStatus(true);
+    } catch (error: any) {
+      console.error(error instanceof Error ? error.message : error);
+      setStatus(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <h1>{breedName}</h1>
+      {status && <BreedImage breedName={breedName} imageUrl={breedImage} />}
+    </div>
+  );
 };
 export default BreedRandomImage;
