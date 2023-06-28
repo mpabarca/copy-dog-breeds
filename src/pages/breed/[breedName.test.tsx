a26c4d06fda016cb9875c6fd393434a9aedf0457
Maria Paz Abarca <mp.abarca.alvear@gmail.com>
2023-06-28 08:58:41 +0200

    Add tests to show-all-breeds and breed pages

---
+++ src/pages/breed/[breedName.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {act, render} from '@testing-library/react';
+import BreedDetailPage from './[breedName]';
+import {getListImagesByBreed} from '../../services/list-images-by-breed';
+
+jest.mock('../../services/list-images-by-breed', () => ({
+  getListImagesByBreed: jest.fn().mockResolvedValue({
+    message: [
+      'https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg',
+      'https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg',
+      'https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg',
+      'https://images.dog.ceo/breeds/hound-afghan/n02088094_10822.jpg',
+    ],
+    status: 'success',
+  }),
+}));
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    query: {
+      breedName: 'hound',
+    },
+  })),
+}));
+
+jest.mock('../../components/template/BreedDetailTemplate/BreedDetailTemplate');
+
+describe('BreedDetailPage test', () => {
+  it('should render BreedDetailPage component', async () => {
+    await act(async () => {
+      const {container} = render(<BreedDetailPage />);
+      expect(container).toBeTruthy();
+    });
+  });
+  it('should call getListImagesByBreed service', async () => {
+    await act(async () => {
+      render(<BreedDetailPage />);
+      expect(getListImagesByBreed).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/pages/show-all-breeds/index.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {act, render} from '@testing-library/react';
+import ListBreedsPage from './index';
+import {getListAllBreeds} from '../../services/list-all-breeds';
+
+jest.mock('../../services/list-all-breeds', () => ({
+  getListAllBreeds: jest.fn().mockResolvedValue({
+    message: {
+      affenpinscher: [],
+      african: [],
+      airedale: [],
+      akita: [],
+      appenzeller: [],
+      australian: ['shepherd'],
+    },
+    status: 'success',
+  }),
+}));
+
+jest.mock('../../components/template/BreedsListTemplate/BreedsListTemplate');
+
+describe('ListBreedsPage test', () => {
+  it('should render ListBreedsPage component', async () => {
+    await act(async () => {
+      const {container} = render(<ListBreedsPage />);
+      expect(container).toBeTruthy();
+    });
+  });
+  it('should call getListAllBreeds service', async () => {
+    await act(async () => {
+      render(<ListBreedsPage />);
+      expect(getListAllBreeds).toHaveBeenCalled();
+    });
+  });
+});
